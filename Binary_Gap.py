3dfe4054c4b0c5af693d5755aec3687a05de4219
bostonbrad <35382621+bostonbrad@users.noreply.github.com>
2018-03-26 22:08:16 -0400

    Update Binary_Gap.py

--- Binary_Gap.py
+++ Binary_Gap.py
@@ -1,5 +1,5 @@
 """
-Task: PermCheck
+Task: BinaryGap
 Goal: Find longest sequence of zeros in binary representation of an integer.
 Website: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
 -----------------------------------------------------------
