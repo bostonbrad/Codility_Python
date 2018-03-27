6f196c68fdd10356ff55c4fbc12d32c5543e97c3
bostonbrad <35382621+bostonbrad@users.noreply.github.com>
2018-03-26 21:23:11 -0400

    Create Binary_Gap.py
    100% Score on Codility

---
+++ Binary_Gap.py
@@ -0,0 +1,50 @@
+"""
+Task: PermCheck
+Goal: Check whether array A is a permutation.
+Website: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+-----------------------------------------------------------
+
+A binary gap within a positive integer N is any maximal sequence of 
+consecutive zeros that is surrounded by ones at both ends in the binary 
+representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary 
+gap of length 2. The number 529 has binary representation 1000010001 and 
+contains two binary gaps: one of length 4 and one of length 3. The number 
+20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps.
+
+Write a function:
+
+def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary 
+gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has 
+binary representation 10000010001 and so its longest binary gap is of length 5.
+
+Assume that:
+
+N is an integer within the range [1..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(log(N));
+expected worst-case space complexity is O(1).
+"""
+
+def solution(N):
+    binary = "{0:b}".format(N) # get binary format
+    split_binary = binary.split("1") # Split into groupings
+    
+    lengths = []
+    
+    split_binary_lenght = len(split_binary)
+    
+    if split_binary[split_binary_lenght - 1] != '': # Check if last element is zeros
+        split_binary = split_binary[:split_binary_lenght-1] # Delete last element if ends in zeros
+    
+    for element in split_binary:
+        lengths.append(element.count('0')) # Append the lenght of each element that contains zeros
+                 
+    return max(lengths) # Return the binary gap 
